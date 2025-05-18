ce502b298f545ef0bf40b6407ad12f3fdb637eb5
Yuxian Wu <12212614@mail.sustech.edu.cn>
2025-05-19 01:11:32 +0800

    管理员审核页面

--- docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsUser.js
+++ docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsUser.js
@@ -15,13 +15,36 @@ angular.module('docs').controller('SettingsUser', function($scope, $state, Resta
       $scope.users = data.users;
     });
   };
-  
+
   $scope.loadUsers();
-  
+
   /**
    * Edit a user.
    */
   $scope.editUser = function(user) {
     $state.go('settings.user.edit', { username: user.username });
+  };
+
+  // 加载注册申请
+  $scope.loadRegisterRequests = function() {
+    Restangular.one('register/request').get().then(function(data) {
+      $scope.registerRequests = data.requests;
+    });
+  };
+  $scope.loadRegisterRequests();
+
+  // 同意申请
+  $scope.acceptRequest = function(id) {
+    Restangular.one('register/request/accept', id).post().then(function() {
+      $scope.loadRegisterRequests();
+      $scope.loadUsers();
+    });
+  };
+  // 拒绝申请
+  $scope.rejectRequest = function(id) {
+    var reason = prompt('请输入拒绝理由');
+    Restangular.all('register/request/reject/' + id).post({ reason: reason }).then(function() {
+      $scope.loadRegisterRequests();
+    });
   };
 });
